faec38a3adb0b9c9e8ba3bf2a4cb20e8eb06bce9
Odis Harkins M4 <odisjamesharkins@gmail.com>
2025-03-27 13:51:36 -0600

    Update

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(public authService: AuthService) {}
 
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 } 
